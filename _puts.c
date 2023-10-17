9a2e376f9bee3d147327e74fa46a60fc1456a191
mazinalla <mazinelsadig8@gmail.com>
2023-10-17 16:46:50 +0200

    upd

---
+++ _puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts - print string
+ *
+ * @c: string
+ * Return: number opf byte
+ */
+int _puts(char *c)
+{
+        int count = 0;
+
+        if (c)
+        {
+                for (count = 0; c[count] != '\0'; count++)
+                {
+                        _putchar(c[count]);
+                }
+        }
+        return (count);
+}
